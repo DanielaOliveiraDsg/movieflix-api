423a42b6af2da2fd7db410655fdb5c2c571dd66b
DanielaOliveiraDsg <danielaoliveiradsg@gmail.com>
2025-12-28 13:29:05 -0800

    feat: express settings

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
+import express from "express";
 
+const port = 3000;
+const app = express();
+
+app.get("/movies", (req, res) => {
+    res.send("Movies list");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`);
+});
