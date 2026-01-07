0b5c5a5cc939e276374a560e698d09f736ceb0a2
DanielaOliveiraDsg <danielaoliveiradsg@gmail.com>
2026-01-06 16:52:12 -0800

    feat: movies pulled from database in ascendant order with genre and language

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma/index.js";
+
+
+
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/movies", (req, res) => {
-    res.send("Movies list");
+app.get("/movies", async (_, res) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: "asc"
+        },
+        include: {
+            genres: true,
+            languages: true
+        }
+    });
+    res.json(movies);
 });
 
 app.listen(port, () => {
